0b026be7fe24187ec00ef18865b49d8c13c05ead
charlie_volpe <volpegames@gmail.com>
2016-01-20 05:49:36 +0000

    Testing out leveleditor.js stuff

--- js/leveleditor.js
+++ js/leveleditor.js
@@ -1,10 +1,14 @@
 // Member Variables
 var m_grid = [];
 var m_tileSize = 60;
+var m_canvas;
+var m_ctx;
 
 // On page load -- Start();
 $(function() {
     makeBlankGrid(4,4);
+    m_canvas = document.getElementById("gameCanvas");
+    m_ctx = m_canvas.getContext("2d");
 });
 
 
@@ -14,6 +18,7 @@ function makeBlankGrid(rows, cols) {
         for( var j = 0; j < cols; j++ ) {
             m_grid[num] = 0;
             // Create a tile grid
+            m_ctx.fillRect(i*m_tileSize,i*m_tileSize,m_tileSize,m_tileSize);
             num++;
         }
     }
