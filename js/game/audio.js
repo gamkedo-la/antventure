840e326a5c094a43341e39ef8c1a57dbce44bfd1
Charlie Volpe <volpegames@gmail.com>
2016-02-10 22:32:32 -0800

    Looping music starts on game start

--- js/game/audio.js
+++ js/game/audio.js
@@ -6,3 +6,5 @@ var audio_squishSoundV2 = new Audio("SFX/squishSoundV2.wav");
 var audio_walkingloop = new Audio("SFX/walkingloop.wav");
 var audio_bigSound = new Audio("SFX/big-sound.mp3");
 var audio_softSound = new Audio("SFX/soft-sound.mp3");
+
+audio_music.loop = true;
