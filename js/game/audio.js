f1740701cdf754cec504c85777ace742a1ef1e69
Charlie Volpe <volpegames@gmail.com>
2016-02-10 22:25:28 -0800

    Started audio

---
+++ js/game/audio.js
@@ -0,0 +1,8 @@
+var audio_music = new Audio("SFX/natec-strainofthought.mp3");
+var audio_crumble = new Audio("SFX/CrumbleBrick.wav");
+var audio_crumbleBreak = new Audio("SFX/CrumbleBrickBreak.wav");
+var audio_squishSoundV1 = new Audio("SFX/squishSoundV1.wav");
+var audio_squishSoundV2 = new Audio("SFX/squishSoundV2.wav");
+var audio_walkingloop = new Audio("SFX/walkingloop.wav");
+var audio_bigSound = new Audio("SFX/big-sound.mp3");
+var audio_softSound = new Audio("SFX/soft-sound.mp3");
